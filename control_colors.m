5b8ee4974ab5b61978556f7463ceb1cb98679f09
Oliver Lord <oliverlord@it032120.users.bris.ac.uk>
2018-02-12 16:17:45 +0000

    Progress commenting IRS.m and craeted new stand alone functions plot_axes and control_colors.

---
+++ control_colors.m
@@ -0,0 +1,7 @@
+function [ output_args ] = control_colors( input_args )
+%CONTROL_COLORS Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ plot_axes.m
@@ -0,0 +1,29 @@
+function [ output_args ] = plot_axes( input_args )
+%--------------------------------------------------------------------------
+% PLOT_AXES
+%--------------------------------------------------------------------------
+% Written and tested on Matlab R2014a (Windows 7) & R2017a (OS X 10.13)
+
+% Copyright 2018 Oliver Lord, Weiwei Wang
+% email: oliver.lord@bristol.ac.uk
+ 
+% This file is part of IRiS.
+ 
+% IRiS is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+ 
+% IRiS is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+ 
+% You should have received a copy of the GNU General Public License
+% along with IRiS.  If not, see <http://www.gnu.org/licenses/>.
+%--------------------------------------------------------------------------
+%   Detailed explanation goes here
+
+
+end
+
